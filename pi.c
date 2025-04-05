3ed80e851e89de9532b3ca0e2c1b7a619f6d0bf1
saulrapos <saul.raposo@udc.es>
2025-04-05 19:52:19 +0200

    p1

--- pi.c
+++ pi.c
@@ -1,36 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <mpi.h>
 
-int main(int argc, char *argv[])
-{
-    int i, done = 0, n, count;
-    double PI25DT = 3.141592653589793238462643;
-    double pi, x, y, z;
+int main(int argc, char *argv[]) {
+    int n, rank, numprocs, count_local, count_total;
+    int i;
+    double x, y, z, pi, PI25DT = 3.141592653589793238462643;
 
-    while (!done)
-    {
-        printf("Enter the number of points: (0 quits) \n");
-        scanf("%d",&n);
-    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+
+    while (1) {
+        if (rank == 0) {
+            printf("Enter the number of points: (0 quits) \n");
+            scanf("%d", &n);
+        }
+
+        // Broadcast n to all processes
+        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
         if (n == 0) break;
 
-        count = 0;  
-
-        for (i = 1; i <= n; i++) {
-            // Get the random numbers between 0 and 1
-	    x = ((double) rand()) / ((double) RAND_MAX);
-	    y = ((double) rand()) / ((double) RAND_MAX);
-
-	    // Calculate the square root of the squares
-	    z = sqrt((x*x)+(y*y));
-
-	    // Check whether z is within the circle
-	    if(z <= 1.0)
-                count++;
+        count_local = 0;
+        for (i = rank; i < n; i += numprocs) {
+            x = ((double) rand()) / ((double) RAND_MAX);
+            y = ((double) rand()) / ((double) RAND_MAX);
+            z = sqrt((x * x) + (y * y));
+            if (z <= 1.0)
+                count_local++;
         }
-        pi = ((double) count/(double) n)*4.0;
 
-        printf("pi is approx. %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
+        // Reduce counts to process 0
+        MPI_Reduce(&count_local, &count_total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+        if (rank == 0) {
+            pi = ((double) count_total / (double) n) * 4.0;
+            printf("pi is approx. %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
+        }
     }
+
+    MPI_Finalize();
+    return 0;
 }
