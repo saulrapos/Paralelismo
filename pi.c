158231977fdb88cdab751905da93d3ad70abb092
saulrapos <saul.raposo@udc.es>
2025-04-06 12:12:14 +0200

    p1

--- pi.c
+++ pi.c
@@ -9,16 +9,16 @@ int main(int argc, char *argv[]) {
     double x, y, z, pi, PI25DT = 3.141592653589793238462643;
     int points_per_process;
     
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);      // id del proceso
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);  // número total de procesos
+    MPI_Init(&argc, &argv);                    //Inicia la colaboración entre procesos
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);      //Id del proceso
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);  //Número total de procesos
 
     if (rank == 0) {
         printf("Enter the number of points: (0 quits) \n");
         scanf("%d", &n);
     }
 
-    // Broadcast n a todos los procesos
+    //Broadcast n a todos los procesos
     MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     if (n == 0) {
@@ -26,10 +26,10 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    // Para asegurar aleatoriedad distinta por proceso
+    //Para asegurar aleatoriedad distinta por proceso
     srand(time(NULL) + rank * 1000);
 
-    // Repartimos el trabajo entre procesos
+    //Repartimos el trabajo entre procesos
     for (i = rank; i < n; i += numprocs) {
         x = ((double) rand()) / RAND_MAX;
         y = ((double) rand()) / RAND_MAX;
@@ -38,14 +38,17 @@ int main(int argc, char *argv[]) {
             count++;
     }
 
-    // Recogida de resultados en el proceso 0
+    //Recogida de resultados en el proceso 0
     MPI_Reduce(&count, &total_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
+
+    //Devolvemos la aproximación de pi y el error
     if (rank == 0) {
         pi = ((double) total_count / (double) n) * 4.0;
         printf("pi is approx. %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
     }
 
+    //Finaliza la colaboración entre procesos
     MPI_Finalize();
     return 0;
 }
