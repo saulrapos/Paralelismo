3b237e8fbfb52788d5be08f5ed3149ceb70ade20
saulrapos <saul.raposo@udc.es>
2025-04-06 11:13:27 +0200

    p1 esta si

--- pi.c
+++ pi.c
@@ -2,43 +2,48 @@
 #include <stdlib.h>
 #include <math.h>
 #include <mpi.h>
+#include <time.h>
 
 int main(int argc, char *argv[]) {
-    int n, rank, numprocs, count_local, count_total;
-    int i;
+    int rank, numprocs, n, i, count = 0, total_count = 0;
     double x, y, z, pi, PI25DT = 3.141592653589793238462643;
-
+    int points_per_process;
+    
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);      // id del proceso
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);  // número total de procesos
 
-    while (1) {
-        if (rank == 0) {
-            printf("Enter the number of points: (0 quits) \n");
-            scanf("%d", &n);
-        }
+    if (rank == 0) {
+        printf("Enter the number of points: (0 quits) \n");
+        scanf("%d", &n);
+    }
 
-        // Broadcast n to all processes
-        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // Broadcast n a todos los procesos
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-        if (n == 0) break;
+    if (n == 0) {
+        MPI_Finalize();
+        return 0;
+    }
 
-        count_local = 0;
-        for (i = rank; i < n; i += numprocs) {
-            x = ((double) rand()) / ((double) RAND_MAX);
-            y = ((double) rand()) / ((double) RAND_MAX);
-            z = sqrt((x * x) + (y * y));
-            if (z <= 1.0)
-                count_local++;
-        }
+    // Para asegurar aleatoriedad distinta por proceso
+    srand(time(NULL) + rank * 1000);
 
-        // Reduce counts to process 0
-        MPI_Reduce(&count_local, &count_total, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    // Repartimos el trabajo entre procesos
+    for (i = rank; i < n; i += numprocs) {
+        x = ((double) rand()) / RAND_MAX;
+        y = ((double) rand()) / RAND_MAX;
+        z = sqrt((x * x) + (y * y));
+        if (z <= 1.0)
+            count++;
+    }
 
-        if (rank == 0) {
-            pi = ((double) count_total / (double) n) * 4.0;
-            printf("pi is approx. %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
-        }
+    // Recogida de resultados en el proceso 0
+    MPI_Reduce(&count, &total_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        pi = ((double) total_count / (double) n) * 4.0;
+        printf("pi is approx. %.16f, Error is %.16f\n", pi, fabs(pi - PI25DT));
     }
 
     MPI_Finalize();
